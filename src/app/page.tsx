63cfe6a6da57e59d8b67cb871eb8441cd87feda5
MaximYakushenkov <maxim@yakushenkov.ru>
2025-07-24 23:17:53 +0300

    feat: DONE

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,25 @@
-import Image from "next/image";
-import styles from "./page.module.css";
-
 export default function Home() {
+
   return (
-   <div>
-    <h1>Hello</h1>
-   </div>
+   <main className="container">
+
+    <img className="avatar" alt="Максим Якушенков, основатель студии yakushenkov.design" src="/avatar.png"/>
+    <div className="title">
+      <span className="desc">твой digital-гуру</span>
+      <h1>Максим Якушенков</h1>
+          <ul>
+      <li>Создаю сайты, которые не просто «красивые», а реально работают и приносят клиентов</li>
+      <li>Упаковываю экспертность и усиливаю онлайн-присутствие брендов</li>
+      <li>Повышаю средний чек и прибыль через сильное позиционирование и UX/UI</li>
+    </ul>
+    </div>
+    <div className="links_container">
+      <a href="https://vk.com/myakushenkov/?utm_source=taplink&utm_medium=social&utm_campgain=таплинк" target="_blank">Вконтакте</a>
+      <a href="https://t.me/yakushenkovm/?utm_source=taplink&utm_medium=social&utm_campgain=таплинк" target="_blank">Телеграм</a>
+      <a href="https://t.me/yakushenkovd/?utm_source=taplink&utm_medium=social&utm_campgain=таплинк" target="_blank">Группа Телеграм</a>
+      <a href="https://vk.com/yakushenkovd/?utm_source=taplink&utm_medium=social&utm_campgain=таплинк" target="_blank">Группа Вконтакте</a>
+      <a href="https://yakushenkovd.ru/?utm_source=taplink&utm_medium=social&utm_campgain=таплинк" target="_blank">Сайт студии <span>yakushenkov.design</span></a>
+    </div>
+   </main>
   );
 }
